27f1c9b8174f44f05df73cfc5d10fd7800de855b
Mabel Zhang <mabel@openrobotics.org>
2023-03-14 22:59:02 -0400

    fix comments
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>

--- docker/run.bash
+++ docker/run.bash
@@ -20,9 +20,9 @@
 # Runs a docker container with the image created by build.bash
 # Requires:
 #   docker
-#   nvidia-docker
 #   an X server
 # Optional:
+#   nvidia-docker
 #   A joystick mounted to /dev/input/js0 or /dev/input/js1
 
 if [ $# -lt 1 ]
@@ -83,7 +83,7 @@ then
   DOCKER_OPTS="$DOCKER_OPTS -v $VIMRC:/home/developer/.vimrc:ro"
 fi
 
-# Mabel: Share your custom terminal setup commands
+# Share your custom terminal setup commands
 GITCONFIG=~/.gitconfig
 DOCKER_OPTS="$DOCKER_OPTS -v $GITCONFIG:/home/developer/.gitconfig:ro"
 
