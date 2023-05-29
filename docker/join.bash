99de2cad515180de248272377415a57450888117
Mabel Zhang <mabel@openrobotics.org>
2023-05-29 02:30:13 +0100

    fix join script for DockerHub image containing slash
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>

--- docker/join.bash
+++ docker/join.bash
@@ -17,10 +17,16 @@
 #
 #
 
-IMG=$(basename $1)
+#IMG=$(basename $1)
 # Use quotes if image name contains symbols like a forward slash /, but then
 # cannot use `basename`.
-#IMG="$1"
+IMG="$1"
+
+# Truncate last "/" in case using bash-complete for directory name
+if [[ $IMG == */ ]]; then
+  IMG=${IMG::-1}
+fi
+echo $IMG
 
 xhost +
 containerid=$(docker ps -aqf "ancestor=${IMG}")
